46b413cf0ff7e44bf44dbb645738f833e79f4175
universmc <hubmaster74@gmail.com>
2023-12-14 17:57:36 +0100

    save model-box

--- pages/projets/P4/src/inc/template/step-by-step/src/inc/step/step2_configuration.php
+++ pages/projets/P4/src/inc/template/step-by-step/src/inc/step/step2_configuration.php
@@ -1,131 +1,125 @@
 <div class="step2_config">
-    <article>
-        <div class="">
-        <div class="code-src">
-            <h3>Resception des sources</h3>
-            <div class="src">                    
-                <div class="init-vscode"></div>
-                <h2>1.Initalisation environnement de develloppemnt</h2>
-                <img src="assets/img/mockup/vscode.png" alt="Image de clonage du répertoire depuis GitHub">
-                <div class="editor">
-
-                    <div>
-                        <?php include "../../../env-edit/env-edit.php";?>
+    <article class="code-src">
+        <div class="step2-1_build-src">
+            <h2>Resception des sources</h2>
+                <!-- 1 vscode 2 terminal 3-1 git clone 3-2 git make repertoire 3-3 nodejs 3-4 git add. -m push -F -->
+                <!-- figma -->
+                <!-- docs + readme.md -->  
+            <div class="env-src">
+                <h3>Step 1 - Initalisation environnement de develloppemnt</h3>                    
+                <div class="init-vscode">
+                    <h4> VsCode</h4></div>
+                    <span></span><br>
+                        <img src="assets/img/mockup/vscode.png" alt="Image de clonage du répertoire depuis GitHub">
                     </div>
-                    <pre class="venv-edit">
-                        <code>git clone https://github.com/univers-mmc/ohmyfood.git</code>
-                    </pre>
-                    <div class="init-git">
-                        <pre>
-                            <code class="c++">make @mkdir src & @cd src > git init -y</code>
-                            <code class="git">repertory universmc/[Ohmyfood].git new /src /build /livr /asset</code>
-                            <code class="shell">touch src/css/style.css build/style/sass-index.scss retaurant/index.html<code>
-                        </pre>
-                    </div>
-                </div>
-
-                    <div class="init-figma3">
-                        <h4>Protoype / maquele</h4>
-                        <div class=img-content>
-                            <img src="../assets/img/figmat.png">
+                    <h3>Terminal intégré</h3>
+                    <div class="editor">
+                        <div class=navbar></div>
+                            <div class="html-css-edit">
+                            <div class="env-html"></div>
+                            <div class="env-css"></div>
+                            </div>
+                            <div class="env-terminal">
+                                <pre>
+                                <div class="cmd_git cmd_node">
+                                   <pre>
+                                        <code>init-clone</code>
+                                        <code>git clone https://github.com/univers-mmc/ohmyfood.git</code>
+                                        <code>init (/) racine repertory</code>
+                                        <code class="git init">git init</code>
+                                        <code class="c++">make @mkdir src & @cd src > git init -y</code>
+                                        <code class="git">repertory universmc/[Ohmyfood].git new /src /build /livr /asset</code>
+                                        <code class="shell">touch src/css/style.css build/style/sass-index.scss retaurant/index.html</code>
+                                        <code class="init nodejs">npm init -y</code>
+                                        <code class="c++">make @mkdir src & @cd src > git init -y</code>
+                                        <code class="git">repertory universmc/[Ohmyfood].git new /src /build /livr /asset</code>
+                                        <code class="shell">touch src/css/style.css build/style/sass-index.scss retaurant/index.html</code>
+                                    </pre>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                        <!-- image figma -->
+                </div>    
+                <div class="init-figma3">
+                    <h4>Protoype / maquele</h4>
+                    <div class=img-content>
+                        <img src="../assets/img/figmat.png">
+                    </div>
+                    <!-- image figma -->
                         <span class="commit">commentaire et description nlp de la maquette</span>
                     </div>
-                    <div class="buidl">
+                </div>
+                <div class="docs">
+                    <h4>docs & assets</h4>
+                        <button>briefing instruction</botton>
+                        <span>Chartre graphique :</span>
                         <h4>text et images</h4>
                         <!-- img du text list flex row list image -->
-                    </div>
-                    <div class="docs">
-                        <div class="readme.md"><h4>git hub readme.md</h4></div>
-                        <h4>Biefing</h4>
-                        <span>Chartre graphique :</span> 
-                        <button>Telecharger biefing</botton>
-                    </div>
+                        <h2>repertory organisation</div>
                 </div>
             </div>
-        </div>
 
-    <h2>Step 2 - Configuration de l'Environnement de Développement</h2>
-        <p>
-            Avant de commencer le projet Ohmyfood, suivez ces étapes pour configurer votre environnement de développement.
-        </p>
-
-        <ol>
-            <li>
-                Clonez le répertoire source du projet à partir de GitHub :
-            </li>
-        </ol>
-
-
-            <ol start="2">
-                <li>
-                    Ouvrez le terminal intégré dans Visual Studio Code (VSCode).
-                </li>
-            </ol>
-            <pre>
-                <code>npm init -y</code>
-            </pre>
-            <img src="chemin/vers/image/init-node-js.png" alt="Image d'initialisation de Node.js">
-
-            <ol start="3">
-                <li>
-                Ajoutez la commande suivante au package.json pour compiler Sass :
-                </li>
-            </ol>
-            <pre>
-                <code>"scripts": {"make-sass": "sass --watch build/sass.scss assets/styles.css"
-                    }</code></pre>
-                            <img src="chemin/vers/image/install-sass-dependency.png" alt="Image d'installation de la dépendance Sass">
-
-                            <ol start="4">
-                                <li>
-                                    Créez les répertoires et fichier nécessaires :
-                                </li>
-                            </ol>
-                            <pre><code>mkdir build assets &&&& cd build && touch sass.scss</code></pre>
-
-                            <ol start="5">
-                                <li>
-                                    Déployez votre première version sur GitHub :
-                                </li>
-                            </ol>
-                            <pre><code>git add .
-                    git commit -m "Initial commit"
-                    git push -u origin main
-                </code>
-            </pre>
+            <div class=step2-env>
+                <div class="sh">terminal-intégré</div>
+                <!-- git add git commit -->
+                <h2>Step 2 - Configuration de l'Environnement de Développement</h2>
+                    <p>
+                        Avant de commencer le projet Ohmyfood, suivez ces étapes pour configurer votre environnement de développement.
+                    </p>
+                <div class="editor env-editor">
+                        <div class=navbar></div>
+                            <div class="html-css-edit">
+                                <div class="env-html"></div>
+                                <div class="env-css"></div>
+                            </div>
+                        <div class="env-terminal">
+                            <pre>
+                            <code>git clone https://github.com/univers-mmc/ohmyfood.git</code>
+                            </pre>
+                            <div class="init-git">
+                                <pre>
+                                    <code class="c++">make @mkdir src & @cd src > git init -y</code>
+                                    <code class="git">repertory universmc/[Ohmyfood].git new /src /build /livr /asset</code>
+                                    <code class="shell">touch src/css/style.css build/style/sass-index.scss retaurant/index.html</code>
+                                    <code>"scripts": {"make-sass": "sass --watch build/sass.scss assets/styles.css"</code>
+                                    <code>mkdir build assets &&&& cd build && touch sass.scss</code>
+                                    <code>git add .</code>
+                                    <code>git commit -m "Initial commit"</code>
+                                    <code>git push -u origin main</code>
+                            </pre>
+                        </div>
             </div>
+
             <div class="plannification">
                 <!-- Template step by step -->
-            <table class="step_template">
-                <div>class event</div>
-                <thead>
-                <tr>
-                <tl><div class="step1-env">1</div><tl>
-                <tl><div class="step2--dev-config">2</div><tl>
-                <tl><div class="step3-html">3</div><tl>
-                <tl><div class="step3-css-sass">4</div><tl>
-                <tl><div class="step4-compiling">5</div><tl>
-                <tl><div class="step3-W3C">6</div><tl>
-                <tl><div class="step3-git">7</div><tl>
-                <tl><div class="step3-integration">8</div><tl>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>#</td>
-                    <td>x</td>
-                    <td>xx</td>
-                    <td>xxx</td>
-                    <td>xxxx</td>
-                    <td>xxxxx</td>
-                    <td>xxxxxx</td>
-                    <td>xxxxxxx</td>
-                    <td>xxxxxxxx</td>
-                <tr>
-                </tbody>
-            </table>
+                <table class="step_template">
+                    <div>class event</div>
+                    <thead>
+                    <tr>
+                        <tl><div class="step1-env">1</div><tl>
+                        <tl><div class="step2--dev-config">2</div><tl>
+                        <tl><div class="step3-html">3</div><tl>
+                        <tl><div class="step3-css-sass">4</div><tl>
+                        <tl><div class="step4-compiling">5</div><tl>
+                        <tl><div class="step3-W3C">6</div><tl>
+                        <tl><div class="step3-git">7</div><tl>
+                        <tl><div class="step3-integration">8</div><tl>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>#</td>
+                            <td>x</td>
+                            <td>xx</td>
+                            <td>xxx</td>
+                            <td>xxxx</td>
+                            <td>xxxxx</td>
+                            <td>xxxxxx</td>
+                            <td>xxxxxxx</td>
+                            <td>xxxxxxxx</td>
+                        <tr>
+                    </tbody>
+                </table>
+            <div class="buidl-step">
             </div>
-    </article>
-</div>
+        </article>
+    </div>
--- pages/projets/P4/src/inc/template/step-by-step/src/inc/step/step4_mockup.php
+++ pages/projets/P4/src/inc/template/step-by-step/src/inc/step/step4_mockup.php
@@ -1,6 +1,8 @@
+User
 <div class="step4_mockup">
     <h2>Step 4 - Découpage de la Maquette</h2>
-        <article>
+    <section class="mockup">
+        <article class="mockup-sementique">
             <p>
                 Le découpage de la maquette est une étape cruciale dans le processus de développement d'un site web.
                 Il s'agit de diviser la conception visuelle en éléments concrets, tels que les en-têtes, les menus, les
@@ -12,44 +14,81 @@
             <p>
                 Le découpage de la maquette consiste à diviser la conception visuelle en éléments concrets, prêts à être traduits en code HTML et CSS. Voici les principaux éléments identifiés pour le site Ohmyfood :
             </p>
-                
-            <img src="assets/img/mockup/mockup.png" class="mockup" alt="mockup">
-                <div class="mockup row mobie">
-                    <div>
-                    <img src="assets/img/mockup/mockup-mobil.png">
-                    </div>
-            
-                <ul>
-                    <li class="mockup-header">
-                        <strong>En-tête (header) :</strong> Logo et barre de navigation.
-                        <!-- Image du Header -->
-                        <img src="assets/img/mockup/mockup-header.png" alt="Header">
-                    </li>
-                    <li class="mockup-main">
-                        <strong>Section Héros (main) :</strong> Bannière avec deux paragraphes captivants et un bouton animé.
-                        <!-- Image de la Section Héros -->
-                        <img src="assets/img/mockup/mockeup-section-hero-ban.png"  alt="Hero Banner">
-                        <!-- Image du Composant Bouton -->
-                    </li>
-                    <li>
-                        <strong>Section Fonctionnement (main) :</strong> Trois boutons interactifs illustrant le fonctionnement.
-                        <!-- Image de la Section Fonctionnement -->
-                        <img src="assets/img/mockup/mockup-section-fonct.png" alt="How It Works">
-                    </li>
-                    <li class="mockup-main">
-                        <strong>Section Restaurants (main) :</strong> Quatre cartes de restaurants avec images, boutons "Nouveau", titres en gras, descriptions et icônes de cœur animées.
-                        <!-- Image de la Section Restaurants -->
-                        <img src="assets/img/mockup/mockup-resentation.png" alt="Restaurants">
-                    </li>
-                    <li class="mockup-main">
-                        <strong>Pied de page (footer) :</strong> Icône Ohmyfood et logos de clients.
-                        <!-- Image du Footer -->
-                        <img src="assets/img/mockup/mockup-footer.png" alt="Footer">
-                        <!-- Image de l'Icône Google Awesome -->
-                        <img src="../../assets/img/mockup/google-awesome.png" alt="Google Awesome">
-                    </li>
-                </ul>
+            <img src="assets/img/mockup/mockup.png" class="mockup-img" alt="mockup">
+        </article>
+    </section>
+    <section class="model-box">
+        <article class="mockup-cutting">
+            <div class="bloc1">
+                <div class="html-body" id="level1">
+                    <span><h3>level1 [HTML] [body] [*]</h3><span>
+                    <img src="assets/img/mockup/mockup-mobil.png" all="media mobile">
+                </div>
             </div>
+            <div class="bloc2">
+                <section class="header-main-footer" id="level2">
+                    <h3>level2 [HEADER][MAIN][FOOTER]</h3>
+                    <article class="ohmyfood-header">[HEADER]</article>
+                    <article class="ohmyfood-main">[MAIN]</article>
+                    <article class="ohmyfood-footer">[FOOTER]</article>
+                </section>
+            </div>
+            <div class="bloc3">
+                <section class="div-section-article" id="level3">
+                    <h3>level3 [DIV]][SECTION][ARTICLE]</h3>
+                    <article class="ohmyfood-header">
+                        <div></div>
+                        <div>[HEADER]</div>
+                        <section>Hero-ban</section>
+                    </article>
+
+                    <article class="ohmyfood-main">
+                            <div>[MAIN]section1</div>
+                            <div>[MAIN]section2</div>
+                    </article>
+                    
+                    <article class="ohmyfood-footer">
+                        <section>
+                            <article>[FOOTER]<article>
+                        </section>
+                    </article>
+                </section>
+
+
+                <section>
+            </div>
+            <div class="bloc4">            
+                <section class="components" id="level4">
+                    <h3>level4 [COMPONNENTS]</h3>
+                    <article class="ohmyfood-header">
+                        <div>[BTN]</div>
+                    </article>
+                    <article class="ohmyfood-main">
+                        <div>[BTN][BTN][BTN]</div>
+                        <div>[CARD][CARD][CARD][CARD]</div>
+                    </article>
+                    <article class="ochmyfood-food"> 
+                            <div>[BTN]</div>
+                    </article>
+                </section>
+            </div>
+            <div class="bloc5">            
+                <section class="div-section-article" id="level3">
+                    <h3>level3 [DIV]][SECTION][ARTICLE]</h3>
+                    <article class="ohmyfood-header">
+                        <div></div>
+                        <div>[DIC]</div>
+                        <div>Hero-ban</div>
+                    </article>
+                    <article class="ohmyfood-main">
+                        <div>[MAIN]section1</div>
+                        <div>[MAIN]section2</div>
+                    </article>
+                    <article class="ohmyfood-footer">
+                        <div>[Article]</div>
+                    </article>
+                <section>
             </div>
         </article>
+    </section>
 </div>
